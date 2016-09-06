28c6d092cc63766677e885e1bb001c5a5526d3b6
Fernando <fernandofbamzn@gmail.com>
2016-09-06 13:18:47 +0200

    Agregar archivos de proyecto.

---
+++ Maquinaria/MainWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿Class MainWindow 
+    Dim bd As clsBaseDatos
+    Public Sub New()
+
+        ' Llamada necesaria para el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        bd = New clsBaseDatos
+
+        datos.DataContext = bd.AbreDataTable("SELECT * FROM TipoContacto ")
+    End Sub
+
+    Private Sub MainWindow_Closing(sender As Object, e As ComponentModel.CancelEventArgs) Handles Me.Closing
+        If Not bd Is Nothing Then bd.close()
+        bd = Nothing
+    End Sub
+End Class
---
+++ Maquinaria/clsBaseDatos.vb
@@ -0,0 +1,102 @@
+﻿Imports System.Data
+Imports MySql.Data.MySqlClient
+Public Class clsBaseDatos
+    Private cnn As MySqlConnection
+    Private cmd As MySqlCommand
+    Private adaptador As MySqlDataAdapter
+
+    Private Function ValorBD(ByVal valor As Object) As String
+        'Funcion que comprueba si un campo tiene valor NULL 
+        'En ese caso devuelve una cadena vacia "", sino devuelve su valor
+        If valor Is System.DBNull.Value Then
+            Return ""
+        ElseIf valor Is Nothing Then
+            Return ""
+        Else
+            Return CStr(valor)
+        End If
+    End Function
+
+    Public Sub New()
+
+        Try
+            Select Case MsgBox("¿La conexión a la Base de Datos es desde la RED LOCAL (Casa)?", MsgBoxStyle.YesNoCancel)
+                Case MsgBoxResult.Yes
+                    abreBaseDatos(True)
+                Case MsgBoxResult.No
+                    abreBaseDatos(False)
+                Case MsgBoxResult.Cancel
+                    My.Application.Shutdown()
+            End Select
+
+
+        Catch ex As Exception
+            MsgBox("Error al conectar con la Base de datos." & vbCrLf & ex.Message & vbCrLf & "Consulta con Fernando.")
+            cnn = Nothing
+            My.Application.Shutdown()
+        End Try
+
+    End Sub
+    Public Sub abreBaseDatos(ByVal isLocal As Boolean)
+        If isLocal Then
+            cnn = New MySqlConnection(datosConexion.CadenaConexion(datosConexion.conexion.Local))
+        Else
+            cnn = New MySqlConnection(datosConexion.CadenaConexion(datosConexion.conexion.Internet))
+        End If
+        cnn.Open()
+        cmd = New MySqlCommand("", cnn)
+        adaptador = New MySqlDataAdapter(cmd)
+    End Sub
+    Public Sub close()
+        If Not cnn Is Nothing Then
+            cnn.Close()
+            cnn = Nothing
+            cmd = Nothing
+            adaptador = Nothing
+        End If
+    End Sub
+
+    Public Function AbreDataTable(ByVal Sql As String) As DataTable
+
+        Dim DTable As New DataTable
+
+        If adaptador.SelectCommand.Connection.State = ConnectionState.Closed OrElse adaptador.SelectCommand.Connection.State = ConnectionState.Broken Then
+            adaptador.SelectCommand.Connection.Open()
+        End If
+        'Se pone la consulta y se carga el datatable
+        adaptador.SelectCommand.CommandText = Sql
+        adaptador.SelectCommand.Parameters.Clear()
+        Try
+            adaptador.Fill(DTable)
+        Catch ex As Exception
+            DTable = New DataTable
+            adaptador.Fill(DTable)
+        End Try
+
+        Return DTable
+
+    End Function
+    Public Function DevuelveValor(ByVal Sql As String) As String
+        '*** El parametro "bConError" indica si al ejecutar la funcion queremos que se muestre
+        '    el mensaje de error si ocurre alguna excepcion.
+        If adaptador.SelectCommand.Connection.State = ConnectionState.Closed OrElse adaptador.SelectCommand.Connection.State = ConnectionState.Broken Then
+            adaptador.SelectCommand.Connection.Open()
+        End If
+        adaptador.SelectCommand.CommandText = Sql
+        adaptador.SelectCommand.Parameters.Clear()
+        Return ValorBD(adaptador.SelectCommand.ExecuteScalar())
+    End Function
+    Public Sub EjecutaConsulta(ByVal sql As String, ByVal params As List(Of KeyValuePair(Of String, Object)))
+        If adaptador.SelectCommand.Connection.State = ConnectionState.Closed OrElse adaptador.SelectCommand.Connection.State = ConnectionState.Broken Then adaptador.SelectCommand.Connection.Open()
+        adaptador.SelectCommand.CommandText = sql
+        adaptador.SelectCommand.Parameters.Clear()
+
+        If Not params Is Nothing Then
+            For Each p As KeyValuePair(Of String, Object) In params
+                adaptador.SelectCommand.Parameters.Add(New MySqlParameter(p.Key, p.Value))
+            Next
+        End If
+
+        adaptador.SelectCommand.ExecuteNonQuery()
+    End Sub
+End Class
